5bcb68669c665f84b4be87254ecdbb8bd467f91d
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2025-05-24 15:35:36 +0300

    Add RoomsController

---
+++ Server/CoWorking.API/Controllers/RoomsController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoWorking.API.Controllers;
+
+[ApiController]
+[Route("api/rooms")]
+public class RoomsController : ControllerBase
+{
+	private readonly IMediator _mediator;
+	public RoomsController(IMediator mediator)
+	{
+		_mediator = mediator;
+	}
+}
