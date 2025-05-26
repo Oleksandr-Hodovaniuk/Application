280c434a8dc54ceb8799aad84726bff4d17c062d
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2025-05-26 13:59:48 +0300

    Update

--- Server/CoWorking.Application/Interfaces/Repositories/IRoomRepository.cs
+++ Server/CoWorking.Application/Interfaces/Repositories/IRoomRepository.cs
@@ -4,5 +4,8 @@ namespace CoWorking.Application.Interfaces.Repositories;
 
 public interface IRoomRepository  
 {
+    /// <summary>
+    /// Returns all rooms with the given workspace type.
+    /// </summary>
     Task<IEnumerable<Room>> GetRoomsByWorkspaceTypeAsync(string workspaceType, CancellationToken cancellationToken);
 }
