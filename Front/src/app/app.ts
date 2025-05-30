d5ed0a663ed21a9e96641e405203d02b58de8c7c
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2025-05-30 20:36:36 +0300

    Create Angular project

--- Front/src/app/app.ts
+++ Front/src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'coworking-client';
+  protected title = 'CoWorkingApp';
 }
