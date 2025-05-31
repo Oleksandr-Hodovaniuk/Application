d7199fa4170f89f0fd5a0ed3a36a70a7a0b8c947
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2025-06-01 02:25:39 +0300

    Create deleteBooking method

--- Front/src/app/services/booking.service.ts
+++ Front/src/app/services/booking.service.ts
@@ -7,11 +7,15 @@ import { BookingModel } from '../models/booking.model';
   providedIn: 'root',
 })
 export class BookingService {
-  private apiUrl = 'https://localhost:7102/api/bookings'; // 
+  private apiUrl = 'https://localhost:7102/api/bookings';
 
   constructor(private http: HttpClient) {}
 
   getAllBookings(): Observable<BookingModel[]> {
     return this.http.get<BookingModel[]>(this.apiUrl);
+  }
+  
+  deleteBooking(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
