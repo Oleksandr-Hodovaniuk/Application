52d0156db750cdbf44615645640451c19b47ab46
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2025-06-01 19:16:01 +0300

    Refactor controller's methods

--- Server/CoWorking.API/Controllers/BookingsController.cs
+++ Server/CoWorking.API/Controllers/BookingsController.cs
@@ -41,7 +41,7 @@ public class BookingsController : ControllerBase
         return Created();
     }
 
-    [HttpGet("id")]
+    [HttpGet("{id}")]
     public async Task<IActionResult> GetByIdAsync(int id, CancellationToken cancellationToken)
     {
         var booking = await _mediator.Send(new GetByIdBookingQuery(id), cancellationToken);
@@ -62,7 +62,7 @@ public class BookingsController : ControllerBase
         return Ok(bookings);
     }
 
-    [HttpPatch("id")]
+    [HttpPatch("{id}")]
     public async Task<IActionResult> PatchAsync(int id,
         [FromBody] JsonPatchDocument<PatchBookingDTO> patchDoc,
         CancellationToken cancellationToken)
